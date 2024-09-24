99cd9e9629e17f78e0a1fac510e124baa343d3ce
ChristopherPillgreen <christopherpillgreen@gmail.com>
2024-09-24 12:59:37 -0400

    For Nick

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -1,9 +1,6 @@
 import { ExerciseCard } from "../components/ExerciseCard";
-import { db } from "@/db";
 
 export default async function Exercises() {
-  const exercises = await db.query.exercisesTable.findMany();
-  console.log(exercises);
   return (
     <div className="flex flex-wrap px-px h-full">
       <ExerciseCard />
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "./components/Sidebar";
-import { ExerciseCard } from "./components/ExerciseCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
