b93185b621d46fb66165c38b29f1e7eb17ba3147
Nick Rouse <nickrouse19@gmail.com>
2025-04-11 11:20:39 -0400

    fixed home hopefully

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { CldImage } from 'next-cloudinary';
 
 export default function Open() {
   return (
-    <div className="relative flex h-screen overflow-hidden bg-white">
+    <div className="relative flex h-screen w-full overflow-hidden bg-white">
       <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
           <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
               <div className="sm:max-w-lg">
